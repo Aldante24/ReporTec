405c27ff040fe3bebfa16e8c1baa7a08f3d15eae
Aldo Martinez Amador <mtz.aldo24@gmail.com>
2017-02-14 20:50:01 -0600

    Se creo formulario
    Se creo vista del formulario de insercion.

--- vista/insertarUsuarios.php
+++ vista/insertarUsuarios.php
@@ -5,6 +5,14 @@
     <title>Insertar</title>
   </head>
   <body>
-
+      <div class="">
+        <fieldset>
+          <label>Usuario:</label>
+          <input type="text" name="usuario">
+          <label>Contraseña:</label>
+          <input type="password" name="pass">
+          <input type="submit">
+        </fieldset>    
+      </div>
   </body>
 </html>
